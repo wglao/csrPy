d706539017a6cb65c54bf7129a24d8dc85002572
wglao <70598852+wglao@users.noreply.github.com>
2022-02-11 11:33:59 -0600

    icepyx reader not working, saving to switch branches

--- atl-download.py
+++ atl-download.py
@@ -17,37 +17,52 @@ import numpy as np
 Modified from icepyx download example: https://github.com/icesat2py/icepyx/blob/main/examples/ICESat-2_DAAC_DataAccess_Example.ipynb
 """
 
-def download(atllist,daterange,region):
+def download(atllist,daterange,region,atlpath):
     """
-    Creates query and downloads matching atl files based upon:
-    - atllist: list of atl product types as a list of strings,
-                    uses all proper atl products. Raises a warning
-                    if invalid product is requested. Raises an
-                    exception if there are no valid products.
-            ex. ['ATL03','ATL08']
+    Creates queries and downloads matching ATL files.
+
+    Parameters
+    -----------
+    - atllist: list of ATL product types as a list of strings,
+                uses all proper atl products. Raises a warning
+                if invalid product is requested. Raises an
+                exception if there are no valid products.
+
+                ex. ['ATL03','ATL08']
+
     - daterange: start and end dates as a list of strings,
-                    formatted as 'YYYY-MM-DD'. If only one date is
-                    given, it is used as the start date and the
-                    current date is used as the end date. Rasies
-                    an exception if the date is in the future or
-                    if more than 2 dates are given.
-            ex. ['2020-01-01','2021-01-01']
-            ex. ['2020-01-01']
+                formatted as 'YYYY-MM-DD'. If only one date is
+                given, it is used as the start date and the
+                current date is used as the end date. Rasies
+                an exception if the date is in the future or
+                if more than 2 dates are given.
+
+                ex. ['2020-01-01','2021-01-01']
+                ex. ['2020-01-01']
+
     - region: name of region as a string, checks region json
-                    for match and pulls down necessary information.
-                    Raises an exception if region is not found.
-            ex. 'New York'
+                for match and pulls down necessary information.
+                Raises an exception if region is not found.
+                
+                ex. 'New York'
+
+    - atlpath: directory used to save downloaded ATL files.
+
+    Returns
+    -------
+    Dictionary containing query objects as values with 
+    corresponding valid ATL products as keys
     """
     
     try:
         # handle ATL list input
-        atlpattern = re.compile(r"ATL\\d\\d")
+        atlpattern = re.compile(r"\AATL\d\d")
         validatls = []
         for atl in atllist:
-            if atlpattern.match(atl, re.IGNORECASE):
-                validatls += atl
-            elif int(atl[4:5]) >= 2 and int(atl[4:5]) <= 21 and int(atl[4:5]) != 18:
-                validatls += atl
+            m = atlpattern.search(atl.upper())
+            if m:
+                if int(atl[4:5]) >= 2 and int(atl[4:5]) <= 21 and int(atl[4:5]) != 18:
+                    validatls += [atl]
             else:
                 warnings.warn("Invalid ATL product. Excluding query.")
         if len(validatls) == 0:
@@ -70,16 +85,24 @@ def download(atllist,daterange,region):
         # TODO: add region recognition
         # New York, New York
         nyspatial = [-74.075,40.67,-73.9,40.825]
+        region = "New York"
 
         # make directory if does not exist
-        regionpath = os.path.join(atlpath,r"New York")
+        regionpath = os.path.join(atlpath,region)
         os.makedirs(regionpath,exist_ok=True)
 
         # download ATL products
+        # define return dict
+        queries = {}
         for atl in validatls:
             query = ipx.Query(atl, nyspatial, daterange)
             query.earthdata_login('wlao', 'wesleygarlao@utexas.edu')
-            query.download_granules(regionpath)
+            # query.download_granules(regionpath)
+
+            # construct return dict
+            queries[atl] = query
+
+        return queries
 
     except Exception as e:
         print(e)
@@ -90,18 +113,9 @@ if __name__ == "main":
     atllist = ['ATL03','ATL08']
     today = datetime.today().strftime('%Y-%m-%d')
     daterange = ['2021-11-13',today]
-
+    region = "New York"
     # default download directory
     atlpath = r"C:\Users\wesle\Documents\CSR\csrPy\ATL files"
 
-    # New York, New York
-    nyspatial = [-74.075,40.67,-73.9,40.825]
-
-    # make directory if does not exist
-    regionpath = os.path.join(atlpath,r"New York")
-    os.makedirs(regionpath,exist_ok=True)
-
-    for atl in atllist:
-        nyc = ipx.Query(atl, nyspatial, daterange)
-        nyc.earthdata_login('wlao', 'wesleygarlao@utexas.edu')
-        nyc.download_granules(regionpath)
+    queries = download(atllist,daterange,region,atlpath)
+    print(queries)
